e3dce5e6726ec281e44ddd9a1c72ea0a3f67d81d
Mugerwa Joseph <41841554+kodek2000@users.noreply.github.com>
2019-01-18 08:03:42 +0300

    Add files via upload

--- js/webpage.js
+++ js/webpage.js
@@ -1,6 +1,10 @@
 $(function()
     {
         $("#navItemsforNavigation").hide();
+        $("#blogStories").hide();
+        $("#information").hide()
+        $("#iconLeft").hide();
+        $("#iconLeft2").hide();
     }
 );
 
@@ -11,9 +15,10 @@ function runEventOne()
 {
     $(function()
         {
-
+            $("#iconMenu").slideToggle(400);
             $("#navigation").slideToggle(1000);
             $("#navItemsforNavigation").slideDown(1000);
+            $("#iconClose").fadeIn(400);
             $("#navItemsforNavigation").animate({
                 height: '630px'
             })
@@ -29,10 +34,68 @@ function runEventTwo()
 {
     $(function()
         {
-
+            $("#iconClose").hide(400);
             $("#navItemsforNavigation").slideUp(1000);
+            $("#iconMenu").toggle(400);
             $("#navigation").show(1000);
 
+        }
+    );
+}
+
+var iconClick = document.getElementById("iconRight");
+iconClick.addEventListener('click', runEventThree)
+
+function runEventThree()
+{
+    $(function()
+        {
+            $("#iconRight").hide(100);
+            $("#iconLeft").show(400);
+            $("#blogStories").slideDown(500);
+
+        }
+    );
+}
+
+var iconClick2 = document.getElementById("iconLeft");
+iconClick2.addEventListener('click', runEventFour)
+
+function runEventFour()
+{
+    $(function()
+        {
+            $("#iconLeft").hide(100);
+            $("#iconRight").show(400);
+            $("#blogStories").slideUp(300);
+        }
+    );
+}
+
+var iconClick3 = document.getElementById("iconRight2");
+iconClick3.addEventListener('click', runEventFive)
+
+function runEventFive()
+{
+    $(function()
+        {
+            $("#iconRight2").hide(100);
+            $("#iconLeft2").show(400);
+            $("#information").slideDown(300);
+        }
+    );
+}
+
+var iconClick4 = document.getElementById("iconLeft2");
+iconClick4.addEventListener('click', runEventSix)
+
+function runEventSix()
+{
+    $(function()
+        {
+            $("#iconLeft2").hide(100);
+            $("#iconRight2").show(400);
+            $("#information").slideUp(300);
         }
     );
 }
